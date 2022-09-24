bd0b15ba248ea2b3596efd696921d199729bbd2a
hanthienduc <hanthienduc.96@gmail.com>
2022-09-24 10:49:54 +0900

    add serive call to shazam api using redux toolkit, add custom style for each songcard and basic style for the error and loader component

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', 'b5efd8ed5emsh1dbdbecbeb8dd80p1920afjsnfdc3610b4171');
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+} = shazamCoreApi;
