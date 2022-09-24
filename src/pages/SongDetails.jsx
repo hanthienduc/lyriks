084c54e5b022b95a08128d388861a2ce50e57969
hanthienduc <hanthienduc.96@gmail.com>
2022-09-24 15:19:44 +0900

    add endpoint to get related songs and add related songs component

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -3,13 +3,39 @@ import { useSelector, useDispatch } from 'react-redux';
 import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
 
 import { setActiveSong, playPause } from '../redux/features/playerSlice';
-import { useGetSongDetailsQuery } from '../redux/services/shazamCore';
+import {
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+} from '../redux/services/shazamCore';
 
 const SongDetails = () => {
   const { songid } = useParams();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
-  const { data: songData, isFetching: isFetchingSongDetails } =
-    useGetSongDetailsQuery({ songid });
+  const dispatch = useDispatch();
+  const {
+    data: songData,
+    isFetching: isFetchingSongDetails,
+    error: songError,
+  } = useGetSongDetailsQuery({ songid });
+
+  const {
+    data: relatedSong,
+    isFetching: isFetchingSongRelated,
+    error: relatedError,
+  } = useGetSongRelatedQuery({ songid });
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = (song, i) => {
+    dispatch(setActiveSong({ song, relatedSong, i }));
+    dispatch(playPause(true));
+  };
+
+  if (isFetchingSongDetails || isFetchingSongRelated) return <Loader title="Searching song details" />;
+
+  if (songError || relatedError) return <Error />;
 
   return (
     <div className="flex flex-col">
@@ -30,6 +56,14 @@ const SongDetails = () => {
           )}
         </div>
       </div>
+
+      <RelatedSongs
+        data={relatedSong}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+        handlePauseClick={handlePauseClick}
+        handlePlayClick={handlePlayClick}
+      />
     </div>
   );
 };
